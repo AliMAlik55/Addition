5c1880e63fa66ce1208b8e2c8b4a920dd43f1c88
AliMAlik55 <89336212+AliMAlik55@users.noreply.github.com>
2021-08-22 13:56:42 +0500

    Add files via upload

---
+++ Addition.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1, num2,total;
+	cout << "Enter the first number : ";
+	cin >> num1;
+	cout << "Enter the second number : ";
+	cin >> num2;
+	total = num1 + num2;
+	cout << "The sum of these number is : "<<total;
+	return 0;
+}
